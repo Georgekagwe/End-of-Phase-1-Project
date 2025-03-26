717a5192a3a58c4edff83073fdb051a72a2d4e15
George <george.kagwe@student.moringaschool.com>
2025-03-26 16:50:29 +0300

    DOM elemnts to be manipulated added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // DOM Elements
+    const continentsContainer = document.getElementById('continents-container');
+    const destinationsContainer = document.getElementById('destinations-container');
+    const destinationsSection = document.getElementById('destinations-section');
+    const continentTitle = document.getElementById('continent-title');
+    const backButton = document.getElementById('back-button');
